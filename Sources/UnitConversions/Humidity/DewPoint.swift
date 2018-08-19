fdbb983d065facf577709a2f0a3fc3e8d51d6363
Michael Housh <mhoush@houshhomeenergy.com>
2018-08-19 10:56:20 -0400

    added dew-point

---
+++ Sources/UnitConversions/Humidity/DewPoint.swift
@@ -0,0 +1,33 @@
+//
+//  DewPoint.swift
+//  UnitConversions
+//
+//  Created by Michael Housh on 8/19/18.
+//
+
+import Foundation
+
+/// Calculate's the Dew-Point based on temperature and humidity.
+public struct DewPoint {
+    
+    public let temperature: Temperature
+    public let humidity: RelativeHumidity
+    
+    public init(temperature: Temperature, humidity: RelativeHumidity) {
+        self.temperature = temperature
+        self.humidity = humidity
+    }
+}
+
+extension DewPoint: Valuable {
+    
+    var value: Double {
+        let naturalLog = log(humidity.value)
+        let t = temperature.convert(to: .celsius).value
+        
+        let value = 243.04 * (naturalLog + ((17.625 * t) / (243.04 + t))) / (17.625 - naturalLog - ((17.625 * t) / (243.04 + t)))
+        
+        return Temperature(value, type: .celsius).convert(to: temperature.type).value
+        
+    }
+}
---
+++ Tests/UnitConversionsTests/DewPointTests.swift
@@ -0,0 +1,32 @@
+//
+//  DewPointTests.swift
+//  UnitConversionsTests
+//
+//  Created by Michael Housh on 8/19/18.
+//
+
+import XCTest
+
+@testable import UnitConversions
+
+
+final class DewPointTests: XCTestCase {
+    
+    let temperature = Temperature(75.0)
+    let humidity = RelativeHumidity(percent: 50.0)
+    var dewPoint: DewPoint {
+        return DewPoint(temperature: temperature, humidity: humidity)
+    }
+    
+    func testDewPoint() {
+        let dp = round(dewPoint.value * 100) / 100
+        XCTAssertEqual(dp, 55.11)
+    }
+    
+    func testDewPointCelsius() {
+        let t = Temperature(23, type: .celsius)
+        let Dp = DewPoint(temperature: t, humidity: humidity)
+        let dp = round(Dp.value)
+        XCTAssertEqual(dp,  12.0)
+    }
+}
